fca72ebc611f1aaf80fb37f54bab4a4457935e95
Kris <140774411+krisgums1@users.noreply.github.com>
2025-08-30 13:20:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("enterBtn").addEventListener("click", function () {
   const errorMsg = document.getElementById("errorMsg");
 
   if (password === "7/06/2025") {
-    window.location.href = "main.html"; // redirect to 2nd page
+    window.location.href = "flower.html"; // redirect to 2nd page
   } else {
     errorMsg.style.display = "block";
   }
