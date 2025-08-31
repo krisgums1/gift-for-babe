280a739448e8426dd4f3943004d9f9c6ef468daf
Kris <140774411+krisgums1@users.noreply.github.com>
2025-08-31 10:18:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById("enterBtn").addEventListener("click", function () {
   const password = document.getElementById("password").value.trim();
   const errorMsg = document.getElementById("errorMsg");
 
-  if (password === "7/06/2025") {
-    window.location.href = "flower.html"; // redirect to 2nd page
+  if (password == "07/06/2025" || "7/06/2025") {
+    window.location.href = "main.html"; // redirect to 2nd page
   } else {
     errorMsg.style.display = "block";
   }
